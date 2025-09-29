8afbc9c4ac8085eb9a4c5dcb88bf530b4a2c0bd0
IvanMakarov3301 <ivanmakarovxy@gmail.com>
2025-09-29 23:27:21 +0300

    Исправить код-стайл

--- src/ru/yandex/javacourse/schedule/tasks/Epic.java
+++ src/ru/yandex/javacourse/schedule/tasks/Epic.java
@@ -1,55 +1,55 @@
 package ru.yandex.javacourse.schedule.tasks;
 
-import static ru.yandex.javacourse.schedule.tasks.TaskStatus.NEW;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import static ru.yandex.javacourse.schedule.tasks.TaskStatus.NEW;
+
 public class Epic extends Task {
-	protected ArrayList<Integer> subtaskIds = new ArrayList<>();
+    protected ArrayList<Integer> subtaskIds = new ArrayList<>();
 
-	public Epic(int id, String name, String description) {
-		super(id, name, description, NEW);
-	}
+    public Epic(int id, String name, String description) {
+        super(id, name, description, NEW);
+    }
 
-	public Epic(String name, String description) {
-		super(name, description, NEW);
-	}
+    public Epic(String name, String description) {
+        super(name, description, NEW);
+    }
 
-	public void addSubtaskId(int id) {
-		if (this.id == id){
+    public void addSubtaskId(int id) {
+        if (this.id == id) {
             return;
         }
-        if(!subtaskIds.contains(id)){
+        if (!subtaskIds.contains(id)) {
             subtaskIds.add(id);
         }
-	}
+    }
 
     public Epic(Epic other) {
         super(other);
         this.subtaskIds = new ArrayList<>(other.subtaskIds);
     }
 
-	public List<Integer> getSubtaskIds() {
-		return subtaskIds;
-	}
+    public List<Integer> getSubtaskIds() {
+        return subtaskIds;
+    }
 
-	public void cleanSubtaskIds() {
-		subtaskIds.clear();
-	}
+    public void cleanSubtaskIds() {
+        subtaskIds.clear();
+    }
 
-	public void removeSubtask(int id) {
-		subtaskIds.remove(Integer.valueOf(id));
-	}
+    public void removeSubtask(int id) {
+        subtaskIds.remove(Integer.valueOf(id));
+    }
 
-	@Override
-	public String toString() {
-		return "Epic{" +
-				"id=" + id +
-				", name='" + name + '\'' +
-				", status=" + status +
-				", description='" + description + '\'' +
-				", subtaskIds=" + subtaskIds +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status=" + status +
+                ", description='" + description + '\'' +
+                ", subtaskIds=" + subtaskIds +
+                '}';
+    }
 }
